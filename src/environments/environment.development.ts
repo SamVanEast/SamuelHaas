abe6f94d40a7f444ed9cfc1c573633c078fc6aa9
Samuel Haas <Samuel03102001@gmail.com>
2023-02-27 12:16:36 +0100

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'samuelhaas',
+    appId: '1:789407221157:web:1aafa92ba98fb9d277544c',
+    storageBucket: 'samuelhaas.appspot.com',
+    apiKey: 'AIzaSyBk8EZHgQ-kPUVPBFYbberAf1av1XZRrUQ',
+    authDomain: 'samuelhaas.firebaseapp.com',
+    messagingSenderId: '789407221157',
+  },};
